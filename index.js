22af1bf4b1ff979daa8c05a95ac7868ffa516f49
Kaustuv Karki <karkikaustuv@gmail.com>
2023-12-01 18:44:58 +0545

    Crypto js basic register route

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
-const userRoute = require("./routes/users.js");
+const userRoute = require("./routes/users.route.js");
+const authRoute = require("./routes/auth.route.js");
 const PORT = 5000;
 
 mongoose
   .connect(process.env.MONGODB_URI)
-  .then(() => console.log("MongoDB connected"))
+  .then(() => console.log("MongoDB connect"))
   .catch((err) => console.log(err));
 
 app.use(express.json());
+app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 
 app.listen(process.env.PORT || PORT, () => {
