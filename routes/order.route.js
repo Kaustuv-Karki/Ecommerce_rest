728111f148c1e69a226a97e1c1fb7c802751ffc6
Kaustuv Karki <karkikaustuv@gmail.com>
2023-12-02 23:11:25 +0545

    base routes completed

--- routes/order.route.js
+++ routes/order.route.js
@@ -1,3 +1,60 @@
 const router = require("express").Router();
+const Order = require("../models/Order.js");
+const {
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken.js");
+
+router.post("/", verifyTokenAndAuthorization, async (req, res) => {
+  const order = new Order(req.body);
+  try {
+    const newOrder = await order.save();
+    res.status(200).json(newOrder);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedOrder = await Order.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedOrder);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Order.findByIdAndDelete(req.params.id);
+    res.status(200).json("Order has been deleted");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const order = await Order.find({ userId: req.params.id });
+    res.status(200).json(order);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const orders = await Order.find();
+    res.status(200).json(orders);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
